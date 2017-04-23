acc08037cf75ab4808599d6f4b2139857d4b73a8
andrii.konovalenko <andreyk6@gmail.com>
2017-04-23 10:04:25 +0300

    Add project files.

---
+++ QuestPlatform.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace QuestPlatform.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ QuestPlatform.Api/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace QuestPlatform.Api.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
